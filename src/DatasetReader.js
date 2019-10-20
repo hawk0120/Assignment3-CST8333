938e8d4f06a61328c13db247369ab491663c2c57
Brady Hawkins <35853045+hawk0120@users.noreply.github.com>
2019-10-20 11:55:46 -0400

    update DatasetReader.js

--- src/DatasetReader.js
+++ src/DatasetReader.js
@@ -1,8 +1,41 @@
 /**
  * @author Brady Hawkins
- * 
+ * @description File handles the business logic 
  */
 
 
-const fileUtil = require("./FileAdapter.js");
-fileUtil.loadFileToArray();
+const fileUtil = require("./FileAdapter");
+const Record = require("./Record");
+
+
+module.exports = {
+
+
+    loadDataset: function loadDataset() {
+        fileUtil.loadFileToArray();
+    },
+
+    writeFile: function writeFile() {
+        fileUtil.writeToFile();
+    },
+
+    findRecord: function findRecords(field, id) {
+        fileUtil.searchRecords(field, id);
+    },
+
+    createNewRecord: function createNewRecord() {
+        const record = new Record();
+        fileUtil.addRecord(record);
+    },
+
+    displayAllRecords: function displayAllRecords() {
+       
+    },
+
+    deleteRecords: function deleteRecords() {
+        record.searchRecords(record);
+        fileUtil.deleteRecords();
+    }
+
+
+}
