c61bc7a37661feec347bed74790cf7d25d86daa0
Brady Hawkins <35853045+hawk0120@users.noreply.github.com>
2019-10-20 13:39:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@
  */
 
 const datasetReader = require("./src/DatasetReader");
-const prompt = require("prompt");
+const readline = require('readline-sync');
 
 
 const loadDataset = 0
@@ -17,33 +17,35 @@ const deleteRecord = 3;
 const updateRecord = 4; 
 const saveFile = 5;
 const exit = 6
-const selection = 0;
-
+var selection = 9;
+var i = 0;
 
 
 
 function menu() {
-prompt.start();
-console.log("*********************************************************");
-console.log("*                                                       *");
-console.log("*           Welcome to the CSV Adapter Project          *");
-console.log("*           Written and Designed by Brady Hawkins       *");
-console.log("*            What would you like to do first?           *");
-console.log("*                                                       *");
-console.log("*********************************************************");
-console.log();
-console.log();
-console.log("0. Load dataset from csv");
-console.log("1. Add a record: ");
-console.log("2. Find a record: ");
-console.log("3. Delete a record: ");
-console.log("4. Update a record: ");
-console.log("5. Save to file: ");
-console.log();
-prompt.get(selection, (err, result) => { 
-    console.log("Enter a number to select action: " + prompt.selection);
-});
-}
+
+                console.log("*********************************************************");
+                console.log("*                                                       *");
+                console.log("*           Welcome to the CSV Adapter Project          *");
+                console.log("*           Written and Designed by Brady Hawkins       *");
+                console.log("*            What would you like to do first?           *");
+                console.log("*                                                       *");
+                console.log("*********************************************************");
+                console.log();
+                console.log();
+                console.log("0. Load dataset from csv");
+                console.log("1. Add a record: ");
+                console.log("2. Find a record: ");
+                console.log("3. Delete a record: ");
+                console.log("4. Update a record: ");
+                console.log("5. Save to file: ");
+                console.log();
+
+               var answer = readline.question("Enter a number to select an action: ");
+               this.selection = answer;
+               console.log(`You selected ${answer}`);
+                
+}               
 
 
 // function getRecordvalues() {
@@ -54,27 +56,32 @@ prompt.get(selection, (err, result) => {
 
 
 
-const i = 0;
-while(i != 9){
-        menu() 
-        switch(selection) {
+
+{
+        menu();
+        switch(i) {
             case loadDataset:
-                    // datasetReader.loadDataset();
+                    datasetReader.loadDataset;
                     break;
             case addRecord:
+                //     this.getRecord();
                     
-                    break;
+                    
             case findRecord:
-                    break;
+                    
             case deleteRecord:
-                    break;
+                   
             case updateRecord:
-                    break;
+                   
             case saveFile:
                     datasetReader.writeFile();
-                    break;
+                    
             case exit:
-                    i=9;
+                    i = 9;
                     break;
-        }
+            default:
+                    i = 9;
+                    break;
+                        
+        } while(i != 9);
 }
