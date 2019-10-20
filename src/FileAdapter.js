5911379b09b47eb76534fadaa23e285b74eef91c
Brady Hawkins <35853045+hawk0120@users.noreply.github.com>
2019-10-20 14:27:33 -0400

    Update FileAdapter.js

--- src/FileAdapter.js
+++ src/FileAdapter.js
@@ -10,66 +10,84 @@ const fs = require("fs");
 const path = require("path");
 
 
-const dataset = [ "provolone", 15];
-const record = new Record(dataset);
+const dataset = [];
+const record = new Record();
 
 const file = './resources/cheese.csv';
 const writer = createCsvWriter({
     path: file,
     header: [ record ]
-    
 });
 
 
 module.exports = {
 
-loadFileToArray: function loadFileToArray() {
-    try{
-        fs.createReadStream(file)
-        .pipe(csvReader())
-        .on('data', (data) =>  dataset.push(data));
-    } catch (error) {
-        console.log("File Loading Error");
-    }
-},
-
-
-/**
- * @description Function writes an Array of the objects to a csv file
- */
-writeToFile: function writeToFile() {
-try {
-  
-    writer  
-      async () => writeRecords(dataset)
-
-    } catch (error) {
-        throw new error();
-    }
-
-},
-
-/**
- * @description Function takes a argument, parses it,
- * searchs array for arg, and returns that row
- * @param field
- * @param id
- */
-searchRecords: function searchRecords(field, id) {
-
-        for(i=0; i< dataset.length; i++) {
-            if(dataset[i].field === id) {
-                return dataset[i];       
+        /**
+         * @description Function loads array from file
+         */
+        loadFileToArray: function loadFileToArray() {
+            try{
+                fs.createReadStream(file)
+                .pipe(csvReader())
+                .on('data', (data) =>  dataset.push(data));
+            } catch (error) {
+                console.log("File Loading Error");
             }
+        },
+
+
+        /**
+         * @description Function writes an Array of objects to a csv file
+         */
+        writeToFile: function writeToFile() {
+        try {
+        
+            writer  
+            async () => writeRecords(dataset)
+                
+            } catch (error) {
+            console.log("Write Unsuccessful");
+            }
+
+        },
+
+        /**
+         * @description Function takes a argument, parses it,
+         * searchs array for arg, and returns that row
+         * @param field
+         * @param id
+         */
+        searchRecords: function searchRecords( id) {
+                for(i=0; i< dataset.length; i++) {
+                    if(dataset[i].cheeseId === id) {
+                        return dataset[i];       
+                    }
+                }
+        },
+
+        /**
+         * @description Function gets array and returns it
+         * @returns dataset
+         */
+        displayRecords: function displayRecords() {
+                return dataset;
+        },
+
+        /**
+         * @description Function removes record from 
+         * @param record 
+         */
+        deleteRecords: function deleteRecords(record) { 
+                const deletedRecord = searchRecords(record.cheeseId);
+                dataset.splice(deletedRecord);
+        },
+
+        /**
+         * @description Function adds record to the end of the dataset array
+         * @param record 
+         */
+        addRecord: function addRecord(record) {
+                dataset.push(record);
         }
-},
-
-deleteRecords: function deleteRecords(record) { 
-        dataset.splice(record);
- },
-
-addRecord: function addRecord(record) {
-    dataset.push(record)
-}
 
 }
